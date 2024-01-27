6dded8a4f3304329edc33ad9627aa4be914346be
Your Name <you@example.com>
2024-01-27 06:43:08 -0800

    Finish invoices routes

--- app.js
+++ app.js
@@ -3,21 +3,21 @@
 
 const express = require("express");
 
-
 const ExpressError = require("./expressError")
 const companiesRoutes = require("./routes/companies");
 const invoicesRoutes = require("./routes/invoices");
 
 const app = express();
 
+
 app.use(express.json());
-app.use("companies", companiesRoutes);
-app.use("invoices", invoicesRoutes);
+app.use("/companies", companiesRoutes);
+app.use("/invoices", invoicesRoutes);
 
 
 /** 404 handler */
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
--- routes/invoices.js
+++ routes/invoices.js
@@ -3,6 +3,10 @@ const db = require("../db");
 const ExpressError = require("./expressError")
 const router = new express.Router();
 
+
+
+
+
 router.get('/', async (req, res, next) => {
     try {
         const result = await db.query(
@@ -18,23 +22,126 @@ router.get('/', async (req, res, next) => {
         return next(err);
       }
     });
+router.get('/:id', async (req, res, next) => {
+    try{
+        const result = await db.query(
+    `SELECT i.id, 
+            i.comp_code, 
+            i.amt, 
+            i.paid, 
+            i.add_date, 
+            i.paid_date, 
+            c.name, 
+            c.description 
+     FROM invoices AS i
+       INNER JOIN companies AS c ON (i.comp_code = c.code)  
+     WHERE id = $1`,
+  [id]);
+  if (result.rows.length === 0) {
+    throw new ExpressError(`No such invoice: ${id}`,404);
+  }
+  const data = result.rows[0];
+  const invoice = {
+    id: data.id,
+    company: {
+      code: data.comp_code,
+      name: data.name,
+      description: data.description,
+    },
+    amt: data.amt,
+    paid: data.paid,
+    add_date: data.add_date,
+    paid_date: data.paid_date,
+  };
 
+  return res.json({"invoice": invoice});
+}
+    catch (err) {
+        return next(err);
+    }
+})
 
-// ## **Step 3: Add Invoices**
+router.post("/", async (req, res, next) => {
+    try {
+      let {comp_code, amt} = req.body;
+  
+      const result = await db.query(
+            `INSERT INTO invoices (comp_code, amt) 
+             VALUES ($1, $2) 
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+          [comp_code, amt]);
+  
+      return res.json({"invoice": result.rows[0]});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+  });
 
-// Add ***routes/invoices.js***. All routes in this file should be prefixed by ***/invoices.***
+router.put("/:id", async (req, res, next) => {
+  try {
+    let {amt, paid} = req.body;
+    let id = req.params.id;
+    let paidDate = null;
 
-// **GET /invoices :** Return info on invoices: like `{invoices: [{id, comp_code}, ...]}`
+    const currResult = await db.query(
+          `SELECT paid
+           FROM invoices
+           WHERE id = $1`,
+        [id]);
 
-// **GET /invoices/[id] :** Returns obj on given invoice.
-// If invoice cannot be found, returns 404. Returns `{invoice: {id, amt, paid, add_date, paid_date, company: {code, name, description}}}`
+    if (currResult.rows.length === 0) {
+      throw new ExpressError(`No such invoice: ${id}`, 404);
+    }
 
-// **POST /invoices :** Adds an invoice. Needs to be passed in JSON body of: `{comp_code, amt}`
-// Returns: `{invoice: {id, comp_code, amt, paid, add_date, paid_date}}`
+    const currPaidDate = currResult.rows[0].paid_date;
 
-// **PUT /invoices/[id] :** Updates an invoice. If invoice cannot be found, returns a 404.
-// Needs to be passed in a JSON body of `{amt}` Returns: `{invoice: {id, comp_code, amt, paid, add_date, paid_date}}`
+    if (!currPaidDate && paid) {
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null
+    } else {
+      paidDate = currPaidDate;
+    }
 
-// **DELETE /invoices/[id] :** Deletes an invoice.If invoice cannot be found, returns a 404. Returns: `{status: "deleted"}` Also, one route from the previous part should be updated:
+    const result = await db.query(
+          `UPDATE invoices
+           SET amt=$1, paid=$2, paid_date=$3
+           WHERE id=$4
+           RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+        [amt, paid, paidDate, id]);
 
-// **GET /companies/[code] :** Return obj of company: `{company: {code, name, description, invoices: [id, ...]}}` If the company given cannot be found, this should return a 404 status response.
+    return res.json({"invoice": result.rows[0]});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+
+});
+
+router.delete("/:id", async function (req, res, next) {
+    try {
+      let id = req.params.id;
+  
+      const result = await db.query(
+            `DELETE FROM invoices
+             WHERE id = $1
+             RETURNING id`,
+          [id]);
+  
+      if (result.rows.length === 0) {
+        throw new ExpressError(`No such invoice: ${id}`, 404);
+      }
+  
+      return res.json({"status": "deleted"});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+  });
+  
+  
+  module.exports = router;
